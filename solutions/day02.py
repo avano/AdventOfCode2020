dc793744f95fe4913eb9fcc491ae040212affe22
Andrej Vano <avano@redhat.com>
2020-12-02 09:45:19 +0100

    [Day02] Add day02

---
+++ solutions/day02.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+with open(os.path.dirname(os.path.realpath(__file__)) + "/input/day02.txt", "r") as f:
+    rules = list(f.readlines())
+
+pattern = re.compile(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$")
+
+
+def solve(part):
+    valid_passwords = 0
+    for rule in rules:
+        search = re.search(pattern, rule)
+        low, high, char, password = (
+            int(search.group(1)),
+            int(search.group(2)),
+            search.group(3),
+            search.group(4),
+        )
+        if part == "part1":
+            valid_passwords += 1 if password.count(char) in range(low, high + 1) else 0
+        else:
+            password = password[low - 1] + password[high - 1]
+            valid_passwords += 1 if password.count(char) == 1 else 0
+    return valid_passwords
+
+
+def part1():
+    return solve("part1")
+
+
+def part2():
+    return solve("part2")
